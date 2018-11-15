8a804d330ed21aa455ef60e33f3dd95d128c8b6a
Dmitriy Khvoia <dmitriyk@techfinancials.com>
2018-11-15 17:48:34 +0200

    Collection added.

--- src/main/java/com/techfinancials/practice/Child.java
+++ src/main/java/com/techfinancials/practice/Child.java
@@ -1,11 +1,19 @@
 package com.techfinancials.practice;
 
+import java.util.List;
+
 public class Child extends Person {
 
-    private Person mother;
-    private Person father;
-    private Person brother;
+    private List<Person> relatives;
     private School school;
+
+    public List<Person> getRelatives() {
+        return relatives;
+    }
+
+    public void setRelatives(List<Person> relatives) {
+        this.relatives = relatives;
+    }
 
     public School getSchool() {
         return school;
@@ -15,35 +23,11 @@ public class Child extends Person {
         this.school = school;
     }
 
-    public Person getMother() {
-        return mother;
-    }
-
-    public void setMother(Person mother) {
-        this.mother = mother;
-    }
-
-    public Person getFather() {
-        return father;
-    }
-
-    public void setFather(Person father) {
-        this.father = father;
-    }
-
-    public Person getBrother() {
-        return brother;
-    }
-
-    public void setBrother(Person brother) {
-        this.brother = brother;
-    }
-
     @Override
     public String toString() {
-        String father = "My father is a " + this.father.getGender() + " and his name is " + this.father.getFirstName() + ".\n";
-        String mother = "My mother is a " + this.mother.getGender() + " and her name is " + this.mother.getFirstName() + ".\n";
-        String brother = "My brother is a " + this.brother.getGender() + " and his name is " + this.brother.getFirstName() + ".\n";
+        String father = "My father is a " + relatives.get(0).getGender() + " and his name is " + relatives.get(0).getFirstName() + ".\n";
+        String mother = "My mother is a " + relatives.get(1).getGender() + " and her name is " + relatives.get(1).getFirstName() + ".\n";
+        String brother = "My brother is a " + relatives.get(2).getGender() + " and his name is " + relatives.get(2).getFirstName() + ".\n";
         String school = "I go to " + this.school.getName() + " № " + this.school.getNumber() + ".\n";
 
         return "Hello! I am a child!\n" + father + mother + brother + school;
